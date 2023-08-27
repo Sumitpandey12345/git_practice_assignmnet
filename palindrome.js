32b44b27c58f01bc4ae55d97bce820a507a783c4
Sumit Pandey <spandey0543@gmail.com>
2023-08-27 15:34:26 +0530

    revert back changes

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,1 @@
-let num = 12321; 
-let reverse = 0; 
-for(let i = num; i > 0; i=Math.floor(i/10)){
-    reverse = reverse* 10 + i % 10;
-}
-if(num === reverse){
-    console.log("Palindrome");
-}else{
-    console.log("Not a palindrome");
-}
+This is palindrome
